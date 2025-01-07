3074142c807e601245c6b91e60f60791cc17a526
Design MacMini II <rtplmacminiii@Designs-Mac-mini.local>
2025-01-07 23:04:26 +0530

    [CREATED] portfolio

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+    return (
+        < header className="header" >
+            <div className="logo">
+                <img src="my-portfolio/src/assets/profile.png" alt="Logo" className="logo-img" />
+            </div>
+            <nav>
+                <ul className="nav-links">
+                    <li><a href="#about"> About</a></li>
+                    <li><a href="#community">Community</a></li>
+                    <li><a href="#course">Course</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#stack">Stack</a></li>
+                </ul>
+            </nav>
+            <button className="contact-btn">Let's Talk</button>
+        </header >
+    )
+}
