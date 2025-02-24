b9f1eee33abb98b896427fdcadd98a82b7efa7a5
Design MacMini II <rtplmacminiii@Designs-Mac-mini.local>
2025-02-24 23:15:34 +0530

    Initial commit

--- src/DesignSystemSection.jsx
+++ src/DesignSystemSection.jsx
@@ -11,20 +11,22 @@ const DesignSystemSection = () => {
             <div className="design-system-image">
                 <img src={designSystemImage} alt="Design System" />
             </div>
-
-            <div className="design-system-left">
-                <p className="design-date">January 2025 - In-progress</p>
-                <h1 className="design-heading">
-                    Built, maintained, and <br /> scaled a tokenized design <br /> system at BotPenguin.
-                </h1>
-            </div>
-            <div className="design-system-right">
-                <ul className="design-list">
-                    <li>Design System Tokenisation</li>
-                    <li>Design System Tooling</li>
-                    <li>Maintaining Component Lifecycle</li>
-                    <li>Design Audits & Checks</li>
-                </ul>
+            {/* Content section */}
+            <div className="design-system-content">
+                <div className="design-system-left">
+                    <p className="design-date">January 2025 - In-progress</p>
+                    <h1 className="design-heading">
+                        Built, maintained, and <br /> scaled a tokenized design <br /> system at BotPenguin.
+                    </h1>
+                </div>
+                <div className="design-system-right">
+                    <ul className="design-list">
+                        <li>Design System Tokenisation</li>
+                        <li>Design System Tooling</li>
+                        <li>Maintaining Component Lifecycle</li>
+                        <li>Design Audits & Checks</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
